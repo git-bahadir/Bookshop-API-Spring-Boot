604e6f8947ee17167e1ecb73f5653d1550195354
Bahadir Durmaz <bahadiracimaz@gmail.com>
2022-03-09 23:03:44 +0300

    database migration scripts and final api tests

---
+++ src/test/java/com/bahadir/bookshopAPI/BookshopAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadir.bookshopAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
